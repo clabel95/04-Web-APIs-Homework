5bc8ef889a8ab76b7ec8e55ddcbce48f0eb2ba60
Clay Abel <clayton.abel.95@gmail.com>
2022-01-04 17:11:39 -0600

    added sudo code for how I think I should do this homework.

---
+++ Assets/js/script.js
@@ -0,0 +1,23 @@
+// need multiple variables to store the following variables.
+//  if the quiz is running.
+//  number of questions answered
+//  score
+//  highscore
+//  time
+//  a variable for each of the 4 possible buttons for answers.
+//  a variable that stores an aray with all the questions.
+//  final score
+
+
+
+//first set the stuff we dont want seen to have a display of none
+
+//define an object with properties being numbers and the values being the corosponding quiz question.
+
+//define an object with properties being numbers and the values being the corosponding quiz answers.
+//add a property called correct and add the number of the correct answer so that we can compare on the go.
+
+//initialize the timer to a default time
+
+//create a function to verify any answer.
+
