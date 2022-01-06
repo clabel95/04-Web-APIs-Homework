47cb0fa746e64c1c1a144d8b7ae79b79d0f20b49
Clay Abel <clayton.abel.95@gmail.com>
2022-01-06 13:58:11 -0600

    updated the javascript so that the quiz questions function properly. each time the user finishes the quiz they can input their initials and it will append to a table. when the user clicks clear scores then the table is cleared. when the user clicks restart then the quiz starts over. I still need to add functionality to the view highscores button in the top left as well as add the functionality where when time reaches 0 then the quiz stops.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,11 @@
 // need multiple variables to store the following variables.
-//  if the quiz is running.
-//  number of questions answered
 //  score
+var correct_answers = 0;
+var total_questions = 5;
 //  highscore
+var table = document.getElementById("high_scores_table");
 //  time
+var timeLeft = 60;
 var timerEl = document.getElementById('time_remaining');
 //  a variable for each of the 4 possible buttons for answers.
 var button_1 = document.getElementById("btn_answer_1");
@@ -14,6 +16,9 @@ var button_4 = document.getElementById("btn_answer_4");
 var question = document.getElementById("quiz_question");
 //  final score
 var q_num = 1;
+
+
+
 
 
 
@@ -74,8 +79,10 @@ const answers_row_4 = {
 //initialize the timer to a default time
 
 // Timer that counts down from 60
+
+
 function countdown() {
-    var timeLeft = 60;
+    // var timeLeft = 60;
   
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
@@ -91,7 +98,7 @@ function countdown() {
         timeLeft--;
       } else {
         // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = '';
+        timerEl.textContent = '0';
         // Use `clearInterval()` to stop the timer
         clearInterval(timeInterval);
         // Call the `displayMessage()` function
@@ -104,10 +111,12 @@ function countdown() {
 
 //this button starts the quiz time and updates the questions and answers for the first question.
 document.querySelector("#btn_start_quiz").addEventListener("click", function(event){
+    countdown();
     document.querySelector("#intro").setAttribute("style", "display:none");
+    document.querySelector("#btn_highscore").setAttribute("style", "display:none");
     document.querySelector("#quiz").setAttribute("style", "display:flex");
     quiz_update(q_num);
-    countdown();
+    
 });
 
 //this function will update the quiz questions and answers depending on the value sent in.
@@ -115,6 +124,10 @@ function quiz_update(i){
     if (i > 5){
         document.querySelector("#quiz").setAttribute("style", "display:none");
         document.querySelector("#results").setAttribute("style", "display:flex");
+        timeLeft= 0;
+        q_num = 1;
+        document.querySelector("#btn_highscore").setAttribute("style", "display:flex");
+        final_score_page();
         //end the quiz
     }else{
         question.textContent = questions[i];
@@ -140,16 +153,17 @@ document.querySelector("#btn_answer_1").addEventListener("click", function(event
     if (q_num == 1||q_num == 4){
         //update the text to say the answer was correct.
         //also add 1 to the variable that holds the number of correct answers.
+        correct_answers = correct_answers + 1;
         document.getElementById("right_wrong").textContent = "Previous answer was Right :)";
         console.log("right")
     }else{
         //update the text to say the answer was wrong.
         document.getElementById("right_wrong").textContent = "Previous answer was Wrong :(";
+        timeLeft=timeLeft-10;
         console.log("wrong")
     }
     q_num = q_num + 1;
     quiz_update(q_num)
-
 });
 
 document.querySelector("#btn_answer_2").addEventListener("click", function(event){
@@ -157,16 +171,17 @@ document.querySelector("#btn_answer_2").addEventListener("click", function(event
     if (q_num == 3){
         //update the text to say the answer was correct.
         //also add 1 to the variable that holds the number of correct answers.
+        correct_answers = correct_answers + 1;
         document.getElementById("right_wrong").textContent = "Previous answer was Right :)";
         console.log("right")
     }else{
         //update the text to say the answer was wrong.
         document.getElementById("right_wrong").textContent = "Previous answer was Wrong :(";
+        timeLeft=timeLeft-10;
         console.log("wrong")
     }
     q_num = q_num + 1;
     quiz_update(q_num)
-
 });
 
 document.querySelector("#btn_answer_3").addEventListener("click", function(event){
@@ -174,16 +189,17 @@ document.querySelector("#btn_answer_3").addEventListener("click", function(event
     if (q_num == 2){
         //update the text to say the answer was correct.
         //also add 1 to the variable that holds the number of correct answers.
+        correct_answers = correct_answers + 1;
         document.getElementById("right_wrong").textContent = "Previous answer was Right :)";
         console.log("right")
     }else{
         //update the text to say the answer was wrong.
         document.getElementById("right_wrong").textContent = "Previous answer was Wrong :(";
+        timeLeft=timeLeft-10;
         console.log("wrong")
     }
     q_num = q_num + 1;
     quiz_update(q_num)
-
 });
 
 document.querySelector("#btn_answer_4").addEventListener("click", function(event){
@@ -191,34 +207,63 @@ document.querySelector("#btn_answer_4").addEventListener("click", function(event
     if (q_num == 5){
         //update the text to say the answer was correct.
         //also add 1 to the variable that holds the number of correct answers.
+        correct_answers = correct_answers + 1;
         document.getElementById("right_wrong").textContent = "Previous answer was Right :)";
         console.log("right")
     }else{
         //update the text to say the answer was wrong.
         document.getElementById("right_wrong").textContent = "Previous answer was Wrong :(";
+        timeLeft=timeLeft-10;
         console.log("wrong")
     }
     q_num = q_num + 1;
     quiz_update(q_num)
-
 });
 
-
-
-
-
-
-
-//create a function to subtract time from the clock if the answer is wrong.
+//update final score 
+function final_score_page(){
+    document.getElementById("score").textContent = "Final score: " + correct_answers + "/" + total_questions;
+}
 
 //code to submit the form 
-document.querySelector("#name_form").addEventListener("click", function(event){
+document.querySelector("#submit_button").addEventListener("click", function(event){
     event.preventDefault();
     var initials_input = document.getElementById("Ini").value;
     document.getElementById("Ini").value = "";
+    document.querySelector("#results").setAttribute("style", "display:none");
 
-// send initials_input into another function to save it to the highscores.
+    document.querySelector("#high_scores_page").setAttribute("style", "display:flex");
+    var table = document.getElementById("high_scores_table");
+    var row = table.insertRow(1);
+    var ini_cell = row.insertCell(0);
+    var score_cell = row.insertCell(1);
+    ini_cell.innerHTML = initials_input;
+    score_cell.innerHTML = correct_answers + "/" + total_questions;
+
+
+    // send initials_input into another function to save it to the highscores.
     console.log(initials_input);
-
-
 });
+
+
+document.querySelector("#restart_quiz").addEventListener("click", function(event){
+    correct_answers = 0;
+    document.getElementById("right_wrong").textContent = "";
+    countdown();
+    document.querySelector("#intro").setAttribute("style", "display:none");
+    document.querySelector("#high_scores_page").setAttribute("style", "display:none");
+    document.querySelector("#btn_highscore").setAttribute("style", "display:none");
+    document.querySelector("#quiz").setAttribute("style", "display:flex");
+    quiz_update(q_num);
+    
+});
+
+document.querySelector("#clear_scores").addEventListener("click", function(event){
+    var remove_rows = document.getElementById("high_scores_table").getElementsByTagName("tr").length;
+    console.log(remove_rows);
+    for (i = 1; i<remove_rows; i++){
+        document.getElementById("high_scores_table").deleteRow(1)
+    }
+
+    
+})
